b1f86cfb36fc64928a2333677480397a38511453
Hafiz <hafizdidarali@gmail.com>
2017-12-19 21:33:38 +0000

    changed from css to scss

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-require('./css/style.css');
+import './scss/main.scss';
 let people = require('./people.js');
 let $ = require('jquery');
 
-console.log('working')
+console.log('working');
 
 $.each(people,function (key,value) {
     $('body').append('<h1>' + value.name + '</h1>');
-})
+});
