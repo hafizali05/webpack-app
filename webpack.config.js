736b1c34c40acb8cc47e94992ca6f97537ca5135
Hafiz <hafizdidarali@gmail.com>
2017-12-19 21:29:36 +0000

    sass loaders and ExtractTextPlugin for seperate css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     entry : './src/app.js',
     output: {
-        path: __dirname + '/dist',
-        filename : 'bundle.js'
+        filename : 'bundle.js',
+        path: path.resolve(__dirname + '/dist')
     },
     module : {
         rules :  [
-            { test: /\.css$/, use: 'style-loader' },
-            { test: /\.css$/, use: 'css-loader' },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
@@ -19,5 +34,11 @@ module.exports = {
                 }
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename:"styles.css",
+            allChunks:true
+        })
+    ]
+};
